b6185fb753c90150523050f1d4d3dc411148a3d6
Krishan <kmkrider1999@gmail.com>
2023-06-11 13:35:23 +0530

    Added Image and Script

--- app/blogpost/about.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const about = () => {
-  return <div>I'm about page build using nextJs</div>;
-};
-
-export default about;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import Script from "next/script";
+import Monk from "../app/Images/monk.png";
 
 export default function Home() {
-  return <main className={styles.main}>Hello there this is krishna</main>;
+  return (
+    <div>
+      {/* <Script> </Script> */}
+      <main className={styles.main}>
+        <h1>Hello there this is my first nextJs blog site</h1>
+        <a href="#">Coding Monks</a>
+        <Image src={Monk} height={300} alt="Logo of the site" />
+      </main>
+    </div>
+  );
 }
