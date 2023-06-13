861b103acc008a3835a6bd3ec2cc290347fe4bae
Krishan <kmkrider1999@gmail.com>
2023-06-14 00:18:48 +0530

    Fetched content from file

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 
-export default function handler(req, res) {
-  fs.readdir("blogs", (err, data) => {
-    console.log(data);
-    if (err) {
-      console.log(err);
-    }
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  const data = await fs.promises.readdir("blogs");
+  const blogContents = [];
+  for (let i = 0; i < data.length; i++) {
+    const fileData = await fs.promises.readFile(`blogs/${data[i]}`, "utf-8");
+    blogContents.push(JSON.parse(fileData));
+  }
+  // console.log(blogContents);
+  res.status(200).json(blogContents);
 }
